8a776ea054af4b2f87b9f55c13c006e9102bea64
mparab01 <infoeastindia1@gmail.com>
2016-10-02 21:01:54 -0500

    Range and Xrange
    https://docs.python.org/3/library/functions.html#func-range

---
+++ Range+and+Xrange.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[3]:
+
+range(0,10)
+
+
+# In[8]:
+
+range(0,100,13)
+
+
+# In[10]:
+
+x = range(0,10)
+print x
+
+
+# In[11]:
+
+x2 = xrange(0,10)
+
+
+# In[12]:
+
+print x2
+
+
+# In[14]:
+
+type(x)
+
+
+# In[15]:
+
+type(x2)
+
+
+# In[ ]:
+
+
+
